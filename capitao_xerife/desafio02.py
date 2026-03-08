d98b334fcbe42b4793cd0ab7bd95529f99a01054
Thomas Sievers <t.sievers07@gmail.com>
2026-03-07 21:49:14 -0300

    Revisão e corrigindo exercício 2

--- capitao_xerife/desafio02.py
+++ capitao_xerife/desafio02.py
@@ -1,33 +1,26 @@
 import pandas as pd
 
 df = pd.read_csv("datasets/dataset_002.csv")
+df.info()
 
-#df.info()
-#print(df.head(5))
-
-nota_presenca = df[(df["Nota"] > 8) & (df["Presenca_%"] > 85)]
-#Resultado deu 3
-#print(len(nota_presenca))
+nota_8 = df[(df["Nota"] > 8) & (df["Presenca_%"] > 85)]
+nota_8 = len(nota_8)
 
 entrega_prazo = df.copy()
 entrega_prazo = entrega_prazo.dropna(subset=["Aluno"])
-entrega_prazo = entrega_prazo[entrega_prazo["Entrega_no_prazo"].str.contains("SIM", case=False)]
-#Resutado deu 47
-#print(len(entrega_prazo))
+entrega_prazo = entrega_prazo[entrega_prazo["Entrega_no_prazo"].str.contains("SIM", case=False, na=False)]
+entrega_prazo = len(entrega_prazo)
 
-nota_media_turma = df.copy()
-nota_media_turma = nota_media_turma.dropna(subset=["Nota"])
-nota_media_turma = nota_media_turma.groupby("Turma")["Nota"].mean().round()
-nota_media_turma = nota_media_turma.max()
-#Resultado deu 6
-#print(nota_media_turma)
+maior_nota_turma = df.copy()
+maior_nota_turma = maior_nota_turma.dropna(subset=["Nota"])
+maior_nota_turma = maior_nota_turma.groupby("Turma")["Nota"].mean().round()
+maior_nota_turma = maior_nota_turma.max()
 
 maior_presenca = df.copy()
 maior_presenca = maior_presenca.dropna(subset=["Presenca_%"])
-maior_presenca = maior_presenca[maior_presenca["Presenca_%"] == 100]
-#Resultado disso deu 6
-maior_presenca = maior_presenca[["Presenca_%", "Nota"]].max().round()
-#print(maior_presenca)
+maior_presenca = maior_presenca.sort_values(by="Presenca_%", ascending=False).head(5)
+maior_presenca = maior_presenca["Nota"].head(2).round()
 
-soma_total = len(nota_presenca) + len(entrega_prazo) + nota_media_turma + maior_presenca["Nota"]
+
+soma_total = nota_8 + entrega_prazo + maior_nota_turma + maior_presenca
 print(soma_total)
